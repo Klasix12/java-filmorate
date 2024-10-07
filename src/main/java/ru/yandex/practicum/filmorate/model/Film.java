4aef1c3f4e76b0f7f58557828135d429efd194e9
Artem Oleshev <sunlightpattern@yandex.ru>
2024-10-07 14:04:33 +0300

    feat: add fields validation

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,34 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+import java.time.Month;
+
+@Data
+@Builder
 public class Film {
+
+    private Long id;
+
+    @NotBlank(message = "Имя не должно быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "Максимальная длина описания фильма 200 символов")
+    private String description;
+
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность должна быть положительным числом")
+    private int duration;
+
+    private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, Month.DECEMBER, 28);
+
+    public static boolean isCorrectReleaseDate(LocalDate releaseDate) {
+        return releaseDate.isAfter(MIN_RELEASE_DATE);
+    }
 }
