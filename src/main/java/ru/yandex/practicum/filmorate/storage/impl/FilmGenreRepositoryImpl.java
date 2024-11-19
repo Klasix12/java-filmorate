6a2d67192fe5ee4e66c4b54430e78d7b9eebe308
Artem Oleshev <sunlightpattern@yandex.ru>
2024-11-19 22:10:12 +0300

    chore: fix code style

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmGenreRepositoryImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmGenreRepositoryImpl.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Repository
 public class FilmGenreRepositoryImpl extends BaseRepository<FilmGenre> implements FilmGenreRepository {
 
-    private final String FIND_ALL_BY_FILM_IDS_QUERY = "SELECT * FROM film_genres WHERE film_id IN";
+    private final String findAllByFilmIdsQuery = "SELECT * FROM film_genres WHERE film_id IN";
 
     public FilmGenreRepositoryImpl(JdbcTemplate jdbc, RowMapper<FilmGenre> mapper) {
         super(jdbc, mapper);
@@ -22,7 +22,7 @@ public class FilmGenreRepositoryImpl extends BaseRepository<FilmGenre> implement
     @Override
     public List<FilmGenre> findAllByFilmIds(Set<Long> ids) {
         String placeholders = String.join(", ", ids.stream().map(id -> "?").toArray(String[]::new));
-        String query = FIND_ALL_BY_FILM_IDS_QUERY + "(" + placeholders + ")";
+        String query = findAllByFilmIdsQuery + "(" + placeholders + ")";
 
         return findMany(query, ids.toArray());
     }
